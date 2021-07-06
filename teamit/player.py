b3eeda210c9738f6c3961d4e1db8359cef70bec7
Itay Elizur <elizur.itay@gmail.com>
2021-07-06 16:26:04 +0300

    Gui and more entities

--- teamit/player.py
+++ teamit/player.py
@@ -1,10 +1,14 @@
 from typing import Dict
 
-from teamit.rank import Rank
+from teamit.rating import Rating
 from teamit.sport_type import SportType
 
 
 class Player:
-    def __init__(self, name: str, initial_score: Dict[SportType, Rank]):
+    def __init__(self, name: str, initial_score: Dict[SportType, Rating]):
         self.name = name
-        self.initial_score = initial_score
+        self.rating = initial_score
+        self._relative_goals = []
+
+    def add_match(self, relative_goal: int):
+        self._relative_goals.append(relative_goal)
